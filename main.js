12d0174eebbde033183577efdc3c525a52a9e376
maria <144173559+mabela2807@users.noreply.github.com>
2025-06-06 19:24:09 +0000

    ide

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i = 0; i < botoes.length; i++ ) {
+for(let i=0; i < botoes.length; i++){
     botoes[i].onclick = function(){
         for(let j=0; j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
@@ -14,20 +14,39 @@ for(let i = 0; i < botoes.length; i++ ) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2025-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2026-01-05T00:00:00");
 
-contadores[0].textContent = ;
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
 
 function calculaTempo(tempoObjetivo){
-let tempoAtual = new Date();
-let tempoFinal = tempoObjetivo- tempoAtual;
-let segundos = Math.floor(tempoFinal / 1000);
-let minutos = Math.floor(segundos / 60);
-let horas = Math.floor(minutos /60);
-let dias = Math.floor(horas /24);
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal /1000);
+    let minutos = Math.floor(segundos /60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+    
+    segundos %= 60;
+    minutos %=60;
+    horas %=24;
 
-segundos%=60;
-minutos %=60;
-horas %=24;
-
-return dias = "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+   if(tempoFinal > 0){
+    return [dias, horas, minutos, segundos];
+   }else{
+    return [0, 0, 0, 0];
+   }
 }
+
+function atualizaCronometro(){
+    for (let i = 0; i < contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+function comecaConometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+comecaConometro();
